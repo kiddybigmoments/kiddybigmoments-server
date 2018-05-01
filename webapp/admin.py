13606fefad4449dbf630e92cba3163ead456f336
José Ramón Alonso Tapia <jose.alonso.programmer@gmail.com>
2018-05-01 21:16:55 +0200

    Minimal customization of Django Admin.

--- webapp/admin.py
+++ webapp/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Kid, Photo, Parents
 
+admin.site.site_header = "Administración del sitio"
+admin.site.site_title = admin.site.site_header
+
 admin.site.register(Kid)
 admin.site.register(Photo)
 admin.site.register(Parents)  # , verbose_name="PA")
